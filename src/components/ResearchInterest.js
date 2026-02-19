0f39e19c1177893e567aad8fee36fee088008dd3
mexiqq <ljw040426@gmail.com>
2026-02-18 20:35:26 -0500

    update

--- src/components/ResearchInterest.js
+++ src/components/ResearchInterest.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const ResearchInterest = () => (
     <div className="mt-6 scroll-mt-20" id="research">
-        <div className="text-blue-800	text-xl font-bold">Research Interest</div>
-        <p className="text-sm text-justify">My research interest primarily focusing on the field of <span className="text-blue-800 font-bold">AI Safety & Security</span> and <span className="text-blue-800 font-bold">AI Efficiency</span>, especially in the topic of <span className="text-blue-800">Alignment</span> and <span className="text-blue-800">Attacks/Defenses</span>.</p>
+        <div className="text-blue-800 text-xl font-bold">Research Interest</div>
+        <p className="text-sm text-justify">My researches primarily focuses on the field of <span className="text-blue-800 font-bold">AI Safety & Security</span> and <span className="text-blue-800 font-bold">AI Efficiency</span>, especially in the topic of <span className="text-blue-800 font-bold">Alignment</span> and <span className="text-blue-800 font-bold">Attacks/Defenses</span>.</p>
     </div>
 );
 
